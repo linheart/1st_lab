1a2430a4216114fa288b42c25f4486775cf97b5f
OneDude <rezepov_peter@mail.ru>
2024-03-15 10:35:41 -0400

    СНОВА ОБНОВИЛ МЕЙН

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,28 @@ fn main(){
         println!("Choose the option:\n1 - check if string is palindrom\n2 - count vowels in string\n3 - make string uppercase\n4 - make string lowercase{}", "\nUse Ctrl + C to stop".bright_purple());
 
         match io::stdin().read_line(&mut operation) {
-        Ok(_) => {println!("{}", "Succesful reading".green())},
-        Err(_) => {println!("{}", "Failed to read a string".red()); continue}
-    }
+            Ok(_) => {println!("{}", "Succesful reading".green())},
+            Err(_) => {println!("{}", "Failed to read a string".red()); continue}
+        }
 
-    match operation.trim().len() {
-        1 => {},
-        _ => {println!("{}", "invalid option".red()); continue}
-    }
+        match operation.trim().len() {
+            1 => {},
+            _ => {println!("{}", "invalid option".red()); continue}
+        }
 
-    println!("{}", "Enter the line for the operation");
-    
-    match io::stdin().read_line(&mut input_str) {
-        Ok(_) => {println!("{}", "Succesful reading".green())},
-        Err(_) => {println!("{}", "Failed to read a string".red()); continue}
-    }
+        println!("{}", "Enter the line for the operation");
+        
+        match io::stdin().read_line(&mut input_str) {
+            Ok(_) => {println!("{}", "Succesful reading".green())},
+            Err(_) => {println!("{}", "Failed to read a string".red()); continue}
+        }
 
-    match operation.as_str().trim() {
-        //"1" => println!("Your string is palindrome: {}", is_palindrome(&input_str)),
-        "2" => println!("{}", format!("There are {} vowels in your text\n", count_vowels(&input_str)).purple()),
-        //"3" => println!("{}", to_uppercase(&input_str)),
-        //"4" => println!("{}", to_lowercase(&input_str)), 
-        _ => println!("{}", "Invalid option".red())
+        match operation.as_str().trim() {
+            //"1" => println!("Your string is palindrome: {}", is_palindrome(&input_str)),
+            "2" => println!("{}", format!("There are {} vowels in your text\n", count_vowels(&input_str)).purple()),
+            //"3" => println!("{}", to_uppercase(&input_str)),
+            //"4" => println!("{}", to_lowercase(&input_str)), 
+            _ => println!("{}", "Invalid option".red())
+        }
     }
-}
 }
