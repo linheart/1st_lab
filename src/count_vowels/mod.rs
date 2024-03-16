84f7494919bfdd64d3682dc21ad15464a30be256
OneDude <rezepov_peter@mail.ru>
2024-03-16 08:13:53 -0400

    добавил форматирование

--- src/count_vowels/mod.rs
+++ src/count_vowels/mod.rs
@@ -1,5 +1,8 @@
-pub fn count_vowels(input_str: &str) -> usize{
+pub fn count_vowels(input_str: &str) -> usize {
     let count;
-    count = input_str.chars().filter(|&c| "aeiouyAEIOUY".contains(c)).count();
+    count = input_str
+        .chars()
+        .filter(|&c| "aeiouyAEIOUY".contains(c))
+        .count();
     return count;
 }
--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,57 @@ use std::io;
 
 use colored::*;
 
-
-fn main(){
+fn main() {
     println!("{}", "========A program for performing an operation on a string========".blue());
-    
-    loop{
+
+    loop {
         let mut input_str = String::new();
         let mut operation = String::new();
-        println!("Choose the option:\n1 - check if string is palindrom\n2 - count vowels in string\n3 - make string uppercase\n4 - make string lowercase{}", "\nUse Ctrl + C to stop".bright_purple());
+        println!(
+            "Choose the option:\n1 - check if string is palindrom\n2 - count vowels in string\n3 - make string uppercase\n4 - make string lowercase{}",
+            "\nUse Ctrl + C to stop".bright_purple()
+        );
 
         match io::stdin().read_line(&mut operation) {
-            Ok(_) => {println!("{}", "Succesful reading".green())},
-            Err(_) => {println!("{}", "Failed to read a string".red()); continue}
+            Ok(_) => {
+                println!("{}", "Succesful reading".green());
+            }
+            Err(_) => {
+                println!("{}", "Failed to read a string".red());
+                continue;
+            }
         }
 
         match operation.trim().len() {
-            1 => {},
-            _ => {println!("{}", "invalid option".red()); continue}
+            1 => {}
+            _ => {
+                println!("{}", "invalid option".red());
+                continue;
+            }
         }
 
         println!("{}", "Enter the line for the operation");
-        
+
         match io::stdin().read_line(&mut input_str) {
-            Ok(_) => {println!("{}", "Succesful reading".green())},
-            Err(_) => {println!("{}", "Failed to read a string".red()); continue}
+            Ok(_) => {
+                println!("{}", "Succesful reading".green());
+            }
+            Err(_) => {
+                println!("{}", "Failed to read a string".red());
+                continue;
+            }
         }
 
         match operation.as_str().trim() {
             //"1" => println!("Your string is palindrome: {}", is_palindrome(&input_str)),
-            "2" => println!("{}", format!("There are {} vowels in your text\n", count_vowels(&input_str)).purple()),
+            "2" =>
+                println!(
+                    "{}",
+                    format!("There are {} vowels in your text\n", count_vowels(&input_str)).purple()
+                ),
             //"3" => println!("{}", to_uppercase(&input_str)),
-            //"4" => println!("{}", to_lowercase(&input_str)), 
-            _ => println!("{}", "Invalid option".red())
+            //"4" => println!("{}", to_lowercase(&input_str)),
+            _ => println!("{}", "Invalid option".red()),
         }
     }
 }
