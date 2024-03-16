7acdc7313a02d612c673b535915d554f35da103e
Alexei <leshamihaylenko@bk.ru>
2024-03-16 22:12:23 +0700

    rust_palindrome

---
+++ palindrome/src/main.rs
@@ -0,0 +1,8 @@
+mod palindrome;
+
+use palindrome::palindrome;
+
+fn main() {
+    let a = palindrome("lol");
+    println!("{}", a);
+}
---
+++ palindrome/src/palindrome/mod.rs
@@ -0,0 +1,5 @@
+use palindrome::is_palindrome;
+
+pub fn palindrome(input_str: &str) -> bool {
+    return is_palindrome(input_str);
+}
