22eeb27094fff6ffd9dd2c678a9e9e186733347b
OneDude <rezepov_peter@mail.ru>
2024-03-24 21:53:22 -0400

    коммент2

--- src/count_vowels/mod.rs
+++ src/count_vowels/mod.rs
@@ -1,7 +1,7 @@
 pub fn count_vowels(input_str: &str) -> usize {
     let count;
     count = input_str
-        .chars()
+        .chars() // создаем итератор по строкам
         .filter(|&c| "aeiouyAEIOUY".contains(c))
         .count(); // считает количество элементов прошедших проверку
     return count;
